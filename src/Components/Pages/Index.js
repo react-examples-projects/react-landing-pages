38a5d3c7edec6700a5bfb2f16320d704f651fea2
znareak <libardojesusrengifo129@gmail.com>
2021-07-15 12:42:56 -0400

    dev: change jumbotron background

--- src/Components/Pages/Index.js
+++ src/Components/Pages/Index.js
@@ -9,12 +9,21 @@ export default function Index() {
 
   return (
     <Container className="my-4 w-75" fluid>
-      <Jumbotron>
+      <Jumbotron className="jumbotron-banner text-white">
         <h3>Minimalist Landing Pages with ReactJS</h3>
-        <p className="text-muted p-0">
+        <p className="p-0">
           A group of landing pages and others web applications with reactjs
           using react-booststrap.
         </p>
+        <small className="d-block">
+          You can see more here:
+          <a
+            href="https://portfolio-znareak.vercel.app/"
+            className="text-reset d-inline-block ml-1 font-weight-bold"
+          >
+            portfolio
+          </a>
+        </small>
       </Jumbotron>
 
       <Row className="mt-5">
@@ -24,22 +33,24 @@ export default function Index() {
               as="a"
               href={project.to}
               lg={4}
-              className="mb-5"
+              className="mb-2 p-0"
               key={project.preview}
               data-aos="fade-up"
               data-aos-delay={i * 100}
               data-aos-duration={i * 100}
               data-aos-offset="0"
             >
-              <Image
-                src={project.preview}
-                className="w-100 h-100 rounded-lg"
-                alt={project.name}
-                fluid
-              />
-              <p className="mt-2 text-muted">
-                <small>{project.title}</small>
-              </p>
+              <div className="bg-white m-1 p-3 py-1 h-100 rounded-lg shadow-sm">
+                <p className="text-muted mb-0">
+                  <small>{project.title}</small>
+                </p>
+                <Image
+                  src={project.preview}
+                  className="w-100 h-100"
+                  alt={project.name}
+                  fluid
+                />
+              </div>
             </Col>
           );
         })}
